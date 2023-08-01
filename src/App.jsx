1461b12c4b98b62fb49fa51053e241c57b3a1ebb
Atif Aiman <alserembani94@gmail.com>
2023-08-01 22:49:13 +0800

    add sass

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="wrapper">
+        <p className="title">Hello React</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
